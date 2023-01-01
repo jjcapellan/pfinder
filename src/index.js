0863117b6c9f462783db4d55afdb6fa3b2b1fb4b
jjcapellan <soycape@hotmail.com>
2023-01-01 18:34:01 +0100

    Fix: start node not included in path

--- src/index.js
+++ src/index.js
@@ -67,8 +67,8 @@ function generatePath(node, x0, y0) {
     let current = node;
     while (current) {
         path.push({ x: current.x, y: current.y });
-        current = current.parent;
         if (current.x == x0 && current.y == y0) break;
+        current = current.parent;
     }
 
     return path.reverse();
