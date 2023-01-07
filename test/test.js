8cd18005054954309c568d7d5d80ce32451f8e7a
jjcapellan <soycape@hotmail.com>
2023-01-08 00:56:20 +0100

    Test: added case 500x500 map

--- test/test.js
+++ test/test.js
@@ -1,10 +1,12 @@
 import process from 'node:process';
 import { makeGrid, getPath } from '../src/index.js';
 import { map4x4, map8x8, map40x40 } from './maps.js';
+import { map500x500 } from './map500x500.js';
 
 const grid4x4 = makeGrid(map4x4);
-const grid8x8 = makeGrid(map8x8);
+const grid8x8 = makeGrid(map8x8, true);
 const grid40x40 = makeGrid(map40x40);
+const grid500x500 = makeGrid(map500x500);
 
 function checkPath(grid, path, x0, y0, x1, y1) {
     let start = path[0];
@@ -45,4 +47,5 @@ test(grid8x8, 0, 0, 5, 6);
 test(grid8x8, 0, 0, 6, 6, true);
 test(grid40x40, 0, 0, 39, 39);
 test(grid40x40, 7, 18, 6, 29);
+test(grid500x500, 6, 12, 499, 499);
 
